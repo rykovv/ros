8ad99c97a6b769c8b4de17cb9e19339839eef0f1
Vladislav Rykov <rykovinternational@gmail.com>
2026-06-21 14:10:52 -0700

    ✏️ fix error api typo

--- include/ros/error.hpp
+++ include/ros/error.hpp
@@ -23,19 +23,19 @@ constexpr field_error_handler<Field> ignore_handler = [](T v) -> T {
 
 template <typename Field, typename T = typename Field::value_type>
     requires std::unsigned_integral<T>
-constexpr auto trancate_handler(T v) -> T {
+constexpr auto truncate_handler(T v) -> T {
     return v & (Field::mask >> Field::lsb());
 }
 
 template <typename Field, typename T = typename Field::value_type>
     requires detail::enumeration<T>
-constexpr auto trancate_handler(T v) -> T {
+constexpr auto truncate_handler(T v) -> T {
     // caution: can be cast to a non-existent enum value
     return static_cast<T>(detail::to_underlying(v) & (Field::mask >> Field::lsb()));
 }
 
 template <typename Field>
-constexpr field_error_handler<Field> handle_field_error = trancate_handler<Field>;
+constexpr field_error_handler<Field> handle_field_error = truncate_handler<Field>;
 
 template <typename Register, typename T = typename Register::value_type>
 using register_error_handler = T (*)(T);
